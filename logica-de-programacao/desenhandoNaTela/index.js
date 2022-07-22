da0ebd90f8a2135beba0c543a043e3abf28d889c
Pedro Nunes Machado <100473453+PedroNnsM@users.noreply.github.com>
2022-07-22 13:06:46 -0300

    inicio de animações

--- logica-de-programacao/desenhandoNaTela/index.js
+++ logica-de-programacao/desenhandoNaTela/index.js
@@ -8,30 +8,31 @@ var cores = ['blue', 'red', 'green', 'orange', 'violet', 'pink', 'white', 'black
 var indiceCorAtual = 0; // começa com blue
 
   var raio = 10;
-
+// funções anonimas associadas diretamente a uma variavel 
 function desenhaCirculo(evento) {
     var x = evento.pageX - tela.offsetLeft;
     var y = evento.pageY - tela.offsetTop;
     
   
     
-    if (evento.shiftKey && evento.altKey) {
+    // if (evento.shiftKey && evento.altKey) {
 
-        alert('Só aperte uma tecla por vez, por favor!');
+    //     alert('Só aperte uma tecla por vez, por favor!');
 
-    } else if(evento.shiftKey && raio + 10 <= 40) {
-        raio = raio + 10;
+    // } else if(evento.shiftKey && raio + 10 <= 40) {
+    //     raio = raio + 10;
 
-    } else if(evento.altKey && raio - 5 >= 10) {
-        raio = raio - 5;
+    // } else if(evento.altKey && raio - 5 >= 10) {
+    //     raio = raio - 5;
 
+    // }
+    if(desenha) {
+        pincel.fillStyle = cores[indiceCorAtual];
+        pincel.beginPath();
+        pincel.arc(x, y, raio, 0, 2 * 3.14);
+        pincel.fill();
+        console.log(x + ',' + y);
     }
-
-    pincel.fillStyle = cores[indiceCorAtual];
-    pincel.beginPath();
-    pincel.arc(x, y, raio, 0, 2 * 3.14);
-    pincel.fill();
-    console.log(x + ',' + y);
 }
 
 tela.onclick = desenhaCirculo;
@@ -47,3 +48,19 @@ function mudaCor() {
 }
 
 tela.oncontextmenu = mudaCor;
+
+tela.onmousemove = desenhaCirculo;
+
+function habilitaDesenhar() {
+
+    desenha = true;
+}
+
+function desabilitaDesenhar() {
+
+    desenha = false;
+}
+
+tela.onmousedown = habilitaDesenhar;
+
+tela.onmouseup = desabilitaDesenhar;
